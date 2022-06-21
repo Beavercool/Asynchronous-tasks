5c3607de2eb389152b9bdb7ab480085e068d5da8
Beavercool <Mr.forest.jake@gmail.com>
2022-06-21 18:32:03 +0300

    Add project files.

---
+++ Array.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+
+  public class Array
+    {
+        public delegate void ArrayHandler(Array sender, ArrayEventArgs e);
+        public event ArrayHandler PercantageChange;
+        public double[] arr;
+        public int Length{ get; }
+        public Array(int length, ArrayHandler percantageChange)
+        {
+            Length = length;
+            PercantageChange += percantageChange; 
+            Init(length);
+
+        }
+
+        private void Init(int length)
+        {
+            arr = new double[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = random.Next(-1000, 1000);
+            }
+        }
+        public double Sum()
+        {
+            double res = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                PercantageChange(this, new ArrayEventArgs(i + 1));
+                res += arr[i];
+
+            }
+            return res;
+        }
+        public async Task<double> SumAsync()
+        {
+            double res = 0;
+            await Task.Run(() => {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    PercantageChange(this, new ArrayEventArgs(i + 1));
+                    res += arr[i];
+
+                }
+                
+            });
+            return res;
+
+        }
+        public void Sort()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                PercantageChange(this, new ArrayEventArgs(i + 1));
+                for (int j = 1; j < arr.Length-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        double temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                    
+                }
+            }
+        }
+        public async void SortAsync()
+        {
+            await Task.Run(() => {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    PercantageChange(this, new ArrayEventArgs(i + 1));
+                    for (int j = 1; j < arr.Length - 1; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            double temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+
+                    }
+                }
+            });
+            
+        }
+        public async void SortDescAsync()
+        {
+            await Task.Run(() => {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    PercantageChange(this, new ArrayEventArgs(i + 1));
+                    for (int j = 1; j < arr.Length - 1; j++)
+                    {
+                        if (arr[j] < arr[j + 1])
+                        {
+                            double temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+
+                    }
+                }
+            });
+
+        }
+        public void SortDesc()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                PercantageChange(this, new ArrayEventArgs(i + 1));
+                for (int j = 1; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        double temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+
+                }
+            }
+        }
+        public double Max()
+        {
+            double max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                PercantageChange(this, new ArrayEventArgs(i + 1));
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        public double Min()
+        {
+            double min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                PercantageChange(this, new ArrayEventArgs(i + 1));
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                    
+                }
+                
+            }
+            return min;
+        }
+
+
+    }
+    public class ArrayEventArgs
+    {
+        public int Iteration { get; }
+        public ArrayEventArgs(int iteration)
+        {
+            Iteration = iteration;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void ProgressBar1Update(Array sender, ArrayEventArgs e)
+        {
+            Dispatcher.Invoke(() => { PrBar1.Value = (e.Iteration * 100 / sender.Length); });
+        }
+
+        public void ProgressBar2Update(Array sender, ArrayEventArgs e)
+        {
+            Dispatcher.Invoke(() => { PrBar2.Value = (e.Iteration * 100 / sender.Length); });
+        }
+
+        private void Button_Click_Stop1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Button_Click_Stop2(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+       
+        private void Button_Click_Start(object sender, RoutedEventArgs e)
+        {
+            Array arr1 = new Array(10000, ProgressBar1Update);
+            Array arr2 = new Array(15000, ProgressBar2Update);
+            arr1.PercantageChange += ProgressBar1Update;
+            arr2.PercantageChange += ProgressBar2Update;
+            arr1.SortAsync();
+            arr2.SortDescAsync();
+
+        }
+    }
+    
+}
